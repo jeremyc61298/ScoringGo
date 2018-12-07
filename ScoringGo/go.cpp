75663e6aabb90f50213b7007dc5af82fcb5c05fb
Jeremy Campbell <jeremyc61298@gmail.com>
2018-12-07 17:22:28 -0600

    Working on inputting the board.

--- ScoringGo/go.cpp
+++ ScoringGo/go.cpp
@@ -6,12 +6,57 @@
 // -------------------------------------------------
 #include <iostream>
 #include <fstream>
+#include <vector>
 using std::cout;
+using std::cerr;
 using std::cin;
 using std::endl;
 using std::ifstream;
 using std::ofstream;
+using std::vector;
+
+class ScoreManager {
+	vector<vector<char>> gameboard;
+	const char WHITE = 'W', BLACK = 'B', UNFILLED = 'U';
+	int numWhite, numBlack;
+public:
+	bool inputBoard(ifstream& fin);
+};
+
+bool ScoreManager::inputBoard(ifstream& fin) {
+	bool moreCases = false;
+	int boardSize;
+	fin >> boardSize;
+
+	gameboard.resize(boardSize, vector<char>(boardSize, UNFILLED));
+	
+	if (boardSize != 0) {
+		fin >> numBlack >> numWhite;
+		for (int i = 0; i < numBlack; i++) {
+
+		}
+
+		moreCases = true;
+	}
+	
+	return moreCases;
+}
 
 int main() {
+	ifstream fin("go.in");
+	if (!fin) {
+		cerr << "Cannot find the input file." << endl;
+		return -1;
+	}
 
+	ofstream fout("go.out");
+	ScoreManager sm;
+	bool moreCases = sm.inputBoard(fin);
+
+	while (moreCases) {
+
+		moreCases = sm.inputBoard(fin);
+		if (moreCases)
+			fout << endl;
+	}
 }
