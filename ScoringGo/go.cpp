06a19a8d2c72bf84fb68b727a68eaccde6b2a8a0
Jeremy Campbell <jeremyc61298@gmail.com>
2018-12-07 12:26:27 -0600

    Init commit

---
+++ ScoringGo/go.cpp
@@ -0,0 +1,17 @@
+// -------------------------------------------------
+// go.cpp
+// Jeremy Campbell
+// Given a final board state, this program will 
+// determine who has won the game.
+// -------------------------------------------------
+#include <iostream>
+#include <fstream>
+using std::cout;
+using std::cin;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+
+int main() {
+
+}
