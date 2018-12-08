4d3a5a5b142bb22b00feff6eadbd7b23d0a95eef
Jeremy Campbell <JCAMPBELL2@harding.edu>
2018-12-07 18:29:18 -0600

    Board is input correctly.

--- ScoringGo/go.cpp
+++ ScoringGo/go.cpp
@@ -2,11 +2,12 @@
 // go.cpp
 // Jeremy Campbell
 // Given a final board state, this program will 
-// determine who has won the game.
+// determine who has won the game of Go.
 // -------------------------------------------------
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 using std::cout;
 using std::cerr;
 using std::cin;
@@ -14,32 +15,61 @@ using std::endl;
 using std::ifstream;
 using std::ofstream;
 using std::vector;
+using std::string;
+
+struct point {
+	int x, y;
+	point() : x(0), y(0) {}
+	point(int x, int y) : x(x), y(y) {}
+};
 
 class ScoreManager {
 	vector<vector<char>> gameboard;
 	const char WHITE = 'W', BLACK = 'B', UNFILLED = 'U';
 	int numWhite, numBlack;
+
+	// Methods
+	void readPoints(ifstream& fin, char color, int numColor);
+
 public:
 	bool inputBoard(ifstream& fin);
+
+	void printGameboard();
 };
 
 bool ScoreManager::inputBoard(ifstream& fin) {
 	bool moreCases = false;
 	int boardSize;
+	gameboard.clear();
 	fin >> boardSize;
-
-	gameboard.resize(boardSize, vector<char>(boardSize, UNFILLED));
 	
 	if (boardSize != 0) {
+		gameboard.resize(boardSize, vector<char>(boardSize, UNFILLED));
 		fin >> numBlack >> numWhite;
-		for (int i = 0; i < numBlack; i++) {
-
-		}
-
+		readPoints(fin, BLACK, numBlack);
+		readPoints(fin, WHITE, numWhite);
 		moreCases = true;
 	}
-	
 	return moreCases;
+}
+
+void ScoreManager::readPoints(ifstream& fin, char color, int numColor) {
+	point curr;
+	for (int i = 0; i < numColor; i++) {
+		fin >> curr.x >> curr.y;
+		gameboard[--curr.x][--curr.y] = color;
+	}
+}
+
+void ScoreManager::printGameboard() {
+	cout << "---------------------------------" << endl;
+	for (vector<char>& row : gameboard) {
+		for (char spot : row) {
+			cout << spot << " ";
+		}
+		cout << endl;
+	}
+	cout << "---------------------------------" << endl;
 }
 
 int main() {
@@ -54,6 +84,7 @@ int main() {
 	bool moreCases = sm.inputBoard(fin);
 
 	while (moreCases) {
+		sm.printGameboard();
 
 		moreCases = sm.inputBoard(fin);
 		if (moreCases)
